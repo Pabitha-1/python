e55990d90e6a5bd2466f711086e5c7c4658a87aa
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-09-05 17:59:56 +0530

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,12 @@
+N=int(input("Num="))
+if(N<1000):
+  a=N
+  cha=0
+  while N>0:
+    ch=N%10
+    cha=cha*10+ch
+    N=N/10
+   if(t==cha):
+    print("It is palindrome")
+   else:
+    print("It is not palindrome")
