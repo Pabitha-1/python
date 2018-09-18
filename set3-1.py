48dc0f62320baae27fd7020d6ac134c8c86ed0fe
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-09-18 17:59:21 +0530

    Add files via upload

---
+++ set3-1.py
@@ -0,0 +1,13 @@
+a=int(input("A="))
+b=int(input("B="))
+c=int(input("C="))
+if(1<=a<=100000):
+    sum=0;
+    i=0;
+    while(i,a):
+            sum=sum+b
+            b=b+c
+            i=i+1
+            print(sum)
+    else:
+            print("invalid")
