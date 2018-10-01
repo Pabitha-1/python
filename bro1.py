83883d5db3f673d6bea358852b9baacc597eff44
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-10-01 17:03:06 +0530

    Add files via upload

---
+++ bro1.py
@@ -0,0 +1,10 @@
+print("The longest comman prefix")
+a=int(input("\nEnter the number:"))
+b=input("\nEnter the string:")
+c=input("\nEnter the string:")
+d=0
+e=list(b)
+while a>0:
+    print(e[d],end="")
+    d=d+1
+    a=a-1
