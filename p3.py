2f4025f31c04742e90a3053f3d2cd6b1de2b6863
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-10-04 17:58:30 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,9 +1,11 @@
-p=input("Input a letter of the alphabet: ")
-
-if p in('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % p)
-elif p=='y':
-	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+a=input("Enter the character:\n")
+if(a.isalpha()==True):
+    n=['a','e','i','o','u']
+    for i in n:
+        if(i==a):    
+            print("The character is vowel")
+            break
+    else:
+        print("consonant")
 else:
-	print("%s is a consonant." % p) 
-	
+    print("Input is invalid")
