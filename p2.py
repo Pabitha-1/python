2ecf3b1c7f8b0c831714232ab4a44de90cd74d86
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-10-04 17:26:27 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,5 +1,12 @@
-n=int(input("Enter a number: "))
-if(num%2)==0:
-   print("{0} is Even".format(num))
+a=input("Enter the value:\n")
+if(a.isalpha()==False):
+    n=int(a)
+    if(n<0):
+        print("Invalid")
+    else:
+        if(n%2==0):
+            print("even")
+        else:
+            print("odd")
 else:
-   print("{0} is Odd".format(num))
+    print("Input is invalid")
