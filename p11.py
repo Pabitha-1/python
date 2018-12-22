a2f89bc35323ce1630a680742612c5dcb4fdae33
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-12-22 12:49:09 +0530

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,8 @@
+print("The Day is Holiday or not")
+h = input()
+if(h in ["Saturday","SATURDAY","saturday","Sunday","sunday","SUNDAY"]):
+    print("yes")
+elif(h in ["MONDAY","Monday","monday","TUESDAY","Tuesday","tuesday","WEDNESDAY","Wednesday","wednesday","THURSDAY","Thursday","thursday","FRIDAY","Friday","friday"]):
+    print("no")
+else:
+    print("Invalid Input")
