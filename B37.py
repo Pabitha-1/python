a419bd3bc1c961189035db4cbdaa64fe9d271717
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-12-28 13:55:44 +0530

    Add files via upload

---
+++ B37.py
@@ -0,0 +1,13 @@
+n1=input("Enter 1st number: ");
+if n1=='a':
+    exit();
+else:
+    n2 = input("Enter 2nd number: ");
+    num1 = int(n1);
+    num2 = int(n2);
+    print("After swapping")
+    swap = num1;
+    num1 = num2;
+    numr2 = swap;
+    print("1st number =",num1)
+    print("2nd number=",num2)
