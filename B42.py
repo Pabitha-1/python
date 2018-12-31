93dfddda87c51e1449b9b39dc53004fba1810f00
Pabitha-1 <41040445+Pabitha-1@users.noreply.github.com>
2018-12-31 14:26:33 +0530

    Create B42.py

---
+++ B42.py
@@ -0,0 +1,11 @@
+string1=str(input())
+string2=str(input())
+len1=len(string1)
+len2=len(string2)
+if(len1==len2):
+    print(string2)
+elif(len1>len2):
+    print(string1)
+else:
+    print(string2)
+   
